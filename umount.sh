ffe2a7a1573adf688277173dc6740d0646bdf92a
MidKnight <romain.bouguet@epitech.eu>
2021-02-15 15:28:49 +0100

    Adding more clarity

--- umount.sh
+++ umount.sh
@@ -12,19 +12,21 @@ red='\e[0;31m'
 neutral='\e[0;m'
 umount -R /mnt
 clear
-echo -e "${red}\nAfter switching off the machine:\n${green}Please remove the installation disk and change the network access mode to bridged.\n\n${neutral}"
-sleep 3
+echo -e "${green}\nAfter switching off the machine:\n${green}Please remove the installation disk.\n\n${neutral}"
+sleep 4
+echo -e "${green}If you're running a Virtual Machine: change the network access mode to `bridged`.\n\n${neutral}"
+sleep 4
 echo -e "${red}On reboot:\n${green}Please run arch_setup.sh to complete the installation.\n\n${neutral}"
 sleep 7
 clear
-echo -e "${red}-------IMMINENT EXTINCTION-------\n\n${neutral}"
+echo -e "${red}------- IMMINENT EXTINCTION -------\n\n${neutral}"
 sleep 1
 clear
-echo -e "${red}-------IMMINENT EXTINCTION .-------\n\n${neutral}"
+echo -e "${red}------- IMMINENT EXTINCTION .------\n\n${neutral}"
 sleep 1
 clear
-echo -e "${red}-------IMMINENT EXTINCTION ..-------\n\n${neutral}"
+echo -e "${red}------- IMMINENT EXTINCTION ..-----\n\n${neutral}"
 sleep 1
 clear
-echo -e "${red}-------IMMINENT EXTINCTION ...-------\n\n${neutral}"
+echo -e "${red}------- IMMINENT EXTINCTION ...----\n\n${neutral}"
 shutdown now
